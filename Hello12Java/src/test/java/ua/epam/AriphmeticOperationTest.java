20ccf957ae4ade3fa210ed32fcaeed9a3655b97c
Vladyslav Shkola <Vladyslav_[CShkola@epam.com>
2016-09-13 16:26:43 +0300

    add 'BeforeClass' annotation to tests

--- Hello12Java/src/test/java/ua/epam/AriphmeticOperationTest.java
+++ Hello12Java/src/test/java/ua/epam/AriphmeticOperationTest.java
@@ -1,6 +1,8 @@
 package ua.epam;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,33 +11,35 @@ import static org.junit.Assert.*;
  * Created by Vladyslav_Shkola on 9/13/2016.
  */
 public class AriphmeticOperationTest {
+    AriphmeticOperation a;
+
+    @BeforeClass
+    public void runT(){
+        a = new AriphmeticOperation();
+    }
     @Test
     public void testAdd() throws Exception {
-        AriphmeticOperation a = new AriphmeticOperation();
-        double result = a.add(2,2);
+        double result = a.add(2, 2);
         Assert.assertTrue(result == 4);
 
     }
 
     @Test
     public void testDeduct() throws Exception {
-        AriphmeticOperation a = new AriphmeticOperation();
         double result = a.deduct(2, 2);
         Assert.assertFalse(result == 2);
     }
 
     @Test
     public void testMult() throws Exception {
-        AriphmeticOperation a = new AriphmeticOperation();
         double result = a.mult(2, 2);
-        if(result != 4) Assert.fail();
+        Assert.assertEquals(result, 4.0, 1e-15);
     }
 
     @Test
     public void testDiv() throws Exception {
-        AriphmeticOperation a = new AriphmeticOperation();
         double result = a.div(2, 2);
-        if(result != 1) Assert.fail();
+        Assert.assertEquals(result, 1.0, 1e-15);
     }
 
 }
