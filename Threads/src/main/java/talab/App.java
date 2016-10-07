7fbd3117600d0143baa36d7f9ee346010e2175e1
Vladyslav Shkola <Vladyslav_Shkola@epam.com>
2016-10-07 12:27:36 +0300

    modify threaad project

--- Threads/src/main/java/talab/App.java
+++ Threads/src/main/java/talab/App.java
@@ -12,32 +12,36 @@ public class App {
 
         for (int i = 0; i < 10; i++) {
             final int value = scanner.nextInt();
-            System.out.println(value);
 
-            new Thread()
-            {
+
+            new Thread(new Runnable() {
                 public void run() {
                     try {
-                        System.out.println(app.factorial(value));
+                        double fact = app.factorial(value);
+                        synchronized (System.out){
+                            System.out.print("Factorial of number " + value);
+                            System.out.println("result is " + fact);
+                        }
+
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
                 }
-            }.start();
+            }).start();
 
         }
     }
 
-    public double factorial(int number) throws InterruptedException {
+    public  double   factorial(int number) throws InterruptedException {
         double fact = 1.0;
         for (int i = 2; i <= number; i++) {
             fact *= i;
-            Thread.sleep(1000);
+            Thread.sleep(200);
         }
         return fact;
     }
